33eea55030dca8f52bb3b2675a047bc39cde13ce
Ravi Bhim <ravi.bhim@yahoo.com>
2018-06-10 12:24:10 -0700

    First commit: Course on building a blockchain by David Katz completed.

---
+++ app/miner.js
@@ -0,0 +1,30 @@
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
+
+class Miner {
+  constructor(blockchain, transactionPool, wallet, p2pServer) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.p2pServer = p2pServer;
+  }
+
+  mine() {
+    const validTransactions = this.transactionPool.validTransactions();
+    validTransactions.push(
+      Transaction.rewardTransaction(this.wallet, Wallet.blockchainWallet())
+    );
+    // create a block consisting of the valid transactions
+    const block = this.blockchain.addBlock(validTransactions);
+    // synchronize the chains in the peer-to-peer server
+    this.p2pServer.syncChains();
+    // clear the transaction pool
+    this.transactionPool.clear();
+    // broadcast to every miner to clear their transaction pools
+    this.p2pServer.broadcastClearTransactions();
+
+    return block;
+  }
+}
+
+module.exports = Miner;
